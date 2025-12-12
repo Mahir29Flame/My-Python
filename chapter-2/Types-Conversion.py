448c21bf03fb1a7d81a940439cedf33873e5038e
Mahir29Flame <zarif467396@gmail.com>
2025-12-12 10:16:32 +0600

    converting types

---
+++ chapter-2/Types-Conversion.py
@@ -0,0 +1,12 @@
+Tempreture = "22.4"
+
+print("type of Tempreture is",type(Tempreture),Tempreture)
+
+Tempreture = float(Tempreture)
+print("type of Tempreture is now",type(Tempreture),Tempreture)
+
+Tempreture = int(Tempreture)
+print("type of Tempreture is now",type(Tempreture),Tempreture)
+
+Tempreture = bool(Tempreture)
+print("type of Tempreture is now",type(Tempreture),Tempreture)
